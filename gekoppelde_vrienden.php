7fdf911c8765f386d02759732ccac9db44906246
Smeets83 <chriskevandesmiske@gmail.com>
2014-12-15 14:10:02 +0100

    ee

---
+++ gekoppelde_vrienden.php
@@ -0,0 +1,18 @@
+<?php
+include_once "forms/header.html";
+?>
+<hgroup>
+ <h2>Gekoppelde vrienden</h2>
+ <h3>Hier ziet u vrienden die zijn gekoppeld aan een of meerdere automerken.</h3>
+
+
+<?php
+//Include verbdinding, inhoud van koppelingen
+include("connectDB.php");
+include("koppelinh.php");
+include_once "forms/footer.html";
+?>
+
+
+
+
